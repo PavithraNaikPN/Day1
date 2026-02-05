9e07d9d5e4669f318f67eadc48356dc2969c8188
PavithraNaikPN <pavithranaikpn@gmail.com>
2026-02-05 10:42:19 +0530

    First commit

--- src/Day4/day4_pythonFundamentals.py
+++ src/Day4/day4_pythonFundamentals.py
@@ -15,24 +15,22 @@ print(student.get("country",0))
 for key, value in student.items():
     print(key,value) 
     
-Purchase={
-    "name":"Laptop",
-    "amount":75000,
-    }
-for name,amount in Purchase.items():
-        print(f"{name} spent ₹{amount}")
-        print("Total:",Purchase.values())
-        n=int(input("Enter the number of custmer:"))
-        user_Purchase={}
-for i in range(n):
-    name=input("Enter the name:")
-    amount=int(input(f"Enter the amount for {name}:"))
-    user_Purchase[name]=amount
-print("Customer Purchases Data:",user_Purchase)
-top_customer=max(user_Purchase,key=user_Purchase.get)
-print("Top Spending Customer:",top_customer)
-Last_customer=min(user_Purchase,key=user_Purchase.get)
-print("Top Spending Customer:",Last_customer)
+#input dictionary from user
+n=int(input("Enter number of customers: "))
+user_purchases={}
+
+for _ in range(n):
+    name=input("Enter customer name: ")
+    amount=int(input(f"Enter purchase amount for {name}: "))
+    user_purchases[name]=amount
+
+print("Customer Purchase Data :",user_purchases)
+
+top_customer=max(user_purchases, key=user_purchases.get)
+print(f"Top Customer: {top_customer} with purchase amount ₹ {user_purchases[top_customer]}")
+
+low_customer=min(user_purchases, key=user_purchases.get)
+print(f"Lowest Customer: {low_customer} with purchase amount ₹ {user_purchases[low_customer]}")
 """
 
 numbers={1,2,3,4}
