1e2c4ddc4c5f173e26edd3ccc9cfc24b9d9bebca
PavithraNaikPN <pavithranaikpn@gmail.com>
2026-02-09 12:02:35 +0530

    message

--- src/Day5/main.py
+++ src/Day5/main.py
@@ -1,11 +1,6 @@
 # main.py
-
 import math_operations
-
-# Calculate 2^10
 result_power = math_operations.power(2, 10)
-
-# Calculate average of list
 numbers = [10, 20, 30, 40]
 result_average = math_operations.average(numbers)
 
